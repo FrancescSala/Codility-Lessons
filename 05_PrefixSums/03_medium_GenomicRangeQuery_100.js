8868236fdd33a85a4fe7e6235c511bdde0ef1598
FrancescSala <fsala.valcarcel@gmail.com>
2022-06-09 08:15:27 +0200

    cosmetic change

--- 05_PrefixSums/03_medium_GenomicRangeQuery_100.js
+++ 05_PrefixSums/03_medium_GenomicRangeQuery_100.js
@@ -5,26 +5,20 @@ function solution(S, P, Q) {
     let closest3ToTheLeft = Array(S.length).fill(0); // idem for 'G'
     closest1ToTheLeft.forEach((elem,index,arr) => {
         let impact = IMPACT_FACTOR[S[index]];
-        if (impact === 1) 
-            arr[index] = index;
-        else if (index === 0) 
-            arr[index] = -1;
+        if (impact === 1) arr[index] = index;
+        else if (index === 0) arr[index] = -1;
         else arr[index] = arr[index-1];
     });
     closest2ToTheLeft.forEach((elem,index,arr) => {
         let impact = IMPACT_FACTOR[S[index]];
-        if (impact === 2) 
-            arr[index] = index;
-        else if (index === 0) 
-            arr[index] = -1;
+        if (impact === 2) arr[index] = index;
+        else if (index === 0) arr[index] = -1;
         else arr[index] = arr[index-1];
     });
     closest3ToTheLeft.forEach((elem,index,arr) => {
         let impact = IMPACT_FACTOR[S[index]];
-        if (impact === 3) 
-            arr[index] = index;
-        else if (index === 0) 
-            arr[index] = -1;
+        if (impact === 3) arr[index] = index;
+        else if (index === 0) arr[index] = -1;
         else arr[index] = arr[index-1];
     });
  
