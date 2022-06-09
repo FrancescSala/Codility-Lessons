a9b42ed90b39b0bf5f59b606a3a0c9f6d88413b8
FrancescSala <fsala.valcarcel@gmail.com>
2022-06-09 19:17:53 +0200

    clarification comment

--- 04_CountingElements/04_medium_MissingInteger_100.js
+++ 04_CountingElements/04_medium_MissingInteger_100.js
@@ -1,7 +1,9 @@
 function solution(A) {
-    let  presence = Array(100000);
+    let  presence = Array(100000);   // note that we do not initialize the rarray
     A.forEach(elem => {if (elem >0) presence[elem] = true});
     let result = presence.findIndex((elem, index) => index > 0 && elem === undefined);
     if (result === -1) result = 100001;
     return result;
 }
+
+// it is efficient in time, but occupies quite a lot of memory as the integer in the array are in the range [1..100000]
