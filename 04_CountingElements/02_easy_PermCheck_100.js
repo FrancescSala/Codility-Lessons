85ca39c5271f92b4e0ca3945ca98303cceef4466
FrancescSala <fsala.valcarcel@gmail.com>
2022-06-10 18:32:53 +0200

    slight simplification after the loop

--- 04_CountingElements/02_easy_PermCheck_100.js
+++ 04_CountingElements/02_easy_PermCheck_100.js
@@ -5,5 +5,8 @@ function solution(A) {
         if (A[i] < 1 || A[i] > N) return 0;
         if (++frequencies[A[i]-1] > 1) return 0;
     }
-    return (frequencies.some(elem => elem !== 1)) ? 0 : 1;
+    //return (frequencies.some(elem => elem !== 1)) ? 0 : 1;  
+    // no need to make the statement above. Arrived to this point it will always evaluate to 1, 
+    // we can  return 1 directly
+    return 1;
 }
