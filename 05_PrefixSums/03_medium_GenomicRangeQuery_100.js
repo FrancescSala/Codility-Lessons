5aedf44a4d628666c5fcd9f09fe841c4c8cc2431
FrancescSala <fsala.valcarcel@gmail.com>
2022-06-10 19:25:41 +0200

    shortened the code a little bit

--- 05_PrefixSums/03_medium_GenomicRangeQuery_100.js
+++ 05_PrefixSums/03_medium_GenomicRangeQuery_100.js
@@ -3,25 +3,19 @@ function solution(S, P, Q) {
     let closest1ToTheLeft = Array(S.length).fill(0); // for each position in S, this will indicate the closest position in S with an 'A'
     let closest2ToTheLeft = Array(S.length).fill(0); // idem for 'C'
     let closest3ToTheLeft = Array(S.length).fill(0); // idem for 'G'
-    closest1ToTheLeft.forEach((elem,index,arr) => {
-        let impact = IMPACT_FACTOR[S[index]];
-        if (impact === 1) arr[index] = index;
-        else if (index === 0) arr[index] = -1;
-        else arr[index] = arr[index-1];
-    });
-    closest2ToTheLeft.forEach((elem,index,arr) => {
-        let impact = IMPACT_FACTOR[S[index]];
-        if (impact === 2) arr[index] = index;
-        else if (index === 0) arr[index] = -1;
-        else arr[index] = arr[index-1];
-    });
-    closest3ToTheLeft.forEach((elem,index,arr) => {
-        let impact = IMPACT_FACTOR[S[index]];
-        if (impact === 3) arr[index] = index;
-        else if (index === 0) arr[index] = -1;
-        else arr[index] = arr[index-1];
-    });
- 
+    function initArray(A,value) {
+        A.forEach((elem,index,arr) => {
+            let impact = IMPACT_FACTOR[S[index]];
+            if (impact === value) arr[index] = index;
+            else if (index === 0) arr[index] = -1;
+            else arr[index] = arr[index-1];
+        });    
+    }
+    initArray(closest1ToTheLeft, 1);
+    initArray(closest2ToTheLeft, 2);
+    initArray(closest3ToTheLeft, 3);
+    // the three above are our prefix sums. Now let's use them
+    
     let resultSet = Array(P.length);
     for (let i = 0; i < P.length; i++) {
         if (closest1ToTheLeft[Q[i]] >= P[i]) resultSet[i] = 1;      //  (closest1ToTheLeft[Q[i]] >= P[i]) means there is an 'A' in the slice
